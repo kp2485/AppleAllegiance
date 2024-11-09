2fd0f751756111170473c68f720a543a6a757ef1
Kyle Peterson <kdpeters@gmail.com>
2024-11-09 14:30:48 -0500

    Initial Commit

---
+++ AppleAllegianceTests/AppleAllegianceTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleAllegianceTests.swift
+//  AppleAllegianceTests
+//
+//  Created by Kyle Peterson on 11/9/24.
+//
+
+import Testing
+@testable import AppleAllegiance
+
+struct AppleAllegianceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
